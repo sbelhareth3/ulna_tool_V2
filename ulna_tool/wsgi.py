57775fb571a3868693743e8604114d935839e42d
sbelhareth3 <sbelhareth3@gatech.edu>
2021-08-04 14:57:56 -0400

    wsgi change

--- ulna_tool/wsgi.py
+++ ulna_tool/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'docker_ulna_tool.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ulna_tool.settings')
 
 application = get_wsgi_application()
