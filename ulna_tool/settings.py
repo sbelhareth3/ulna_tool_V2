954b9b4dd849eb58dda4cbc91c3e08fcb200fa21
sbelhareth3 <sbelhareth3@gatech.edu>
2021-08-05 20:09:27 -0400

    new allowed hosts

--- ulna_tool/settings.py
+++ ulna_tool/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0h7fr#%^p9sc1t55ngwq-liqqyt3sch_9a13bsz$c@bo!y7b8y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ulna-pft.herokuapp.com']
 
 
 # Application definition
